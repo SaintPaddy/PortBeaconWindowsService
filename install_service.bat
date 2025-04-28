974fd963de24cef9f684a733710f0daf87c4f24f
SaintPaddy <19346329+SaintPaddy@users.noreply.github.com>
2025-04-28 12:47:03 +0200

    Update install_service.bat

--- install_service.bat
+++ install_service.bat
@@ -3,7 +3,7 @@ sc stop PortBeaconService
 sc delete PortBeaconService
 
 sc create PortBeaconService binPath= "%~dp0WindowsServicePortBeacon.exe" start= auto
-sc description PortBeaconService "Port Beacon Service v1.0 - Created by Patrick - https://github.com/SaintPaddy/WindowsServicePortBeacon"
+sc description PortBeaconService "Port Beacon Service v1.0 - https://github.com/SaintPaddy/PortBeaconWindowsService"
 sc start PortBeaconService
 
 echo.
